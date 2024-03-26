640cbd1d6f5b23c9dce0813b8f61c323120aa783
MRxwr <nasserhatab@gmail.com>
2024-03-27 01:58:45 +0300

    Update config.php

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -9,5 +9,13 @@ die("Connection Failed: " .$dbconnect->connect_error );
 }
 $sql = "SET CHARACTER SET utf8mb4";
 $dbconnect->query($sql);
+
+if( isset($_GET["storeTitle"]) && !empty($_GET["storeTitle"]) ){
+    $check = selectDB("settings","`title` LIKE '%{$_GET["storeTitle"]}%'");
+    if( !isset($check[0]["id"]) ){
+        header("LOCATION: new.createstore.link");die();
+    }
+}
+
 ?>
  
